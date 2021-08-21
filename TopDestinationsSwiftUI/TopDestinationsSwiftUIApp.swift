a0fecd494b0807a7f8c9a72ed753f0578298c82f
Jon Insley <5669143+jinsley8@users.noreply.github.com>
2021-08-20 21:56:15 -0400

    Initial Commit

---
+++ TopDestinationsSwiftUI/TopDestinationsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopDestinationsSwiftUIApp.swift
+//  TopDestinationsSwiftUI
+//
+//  Created by Jonathon Insley on 2021-08-20.
+//
+
+import SwiftUI
+
+@main
+struct TopDestinationsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
